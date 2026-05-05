bae4dccd264622a357fdb4b1b52e0abb62f2e18a
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-05-05 06:11:09 +0300

    Update delete_dnodeint_at_index.c

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,1 +1,53 @@
+#include "lists.h"
+#include <stdlib.h>
 
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index
+ * @head: A pointer to the pointer of the first element
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	tmp = *head;
+
+	/* Case 1: Deleting the head node (index 0) */
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+
+	/* Traverse to find the node at the specified index */
+	while (tmp != NULL && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	/* Case 2: Index out of range */
+	if (tmp == NULL)
+		return (-1);
+
+	/* Case 3: Deleting a node in the middle or at the end */
+	/* Link the previous node to the next node */
+	if (tmp->prev != NULL)
+		tmp->prev->next = tmp->next;
+
+	/* Link the next node back to the previous node */
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev;
+
+	free(tmp);
+	return (1);
+}
