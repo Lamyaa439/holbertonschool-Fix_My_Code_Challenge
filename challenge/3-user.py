1dc1e34961b7a8567e6c9b97bd0515e393f96709
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-05-05 06:04:20 +0300

    Update 3-user.py

--- challenge/3-user.py
+++ challenge/3-user.py
@@ -8,29 +8,55 @@ import uuid
 
 class User():
     """
-    User class
+    User class:
+    - id: public string unique (uuid)
+    - password: private string hash in MD5
     """
 
     __password = None
 
     def __init__(self):
+        """
+        Initialize a new user
+        """
         self.id = str(uuid.uuid4())
 
     @property
     def password(self):
+        """
+        Password getter
+        """
         return self.__password
 
     @password.setter
     def password(self, pwd):
+        """
+        Password setter
+        """
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            # Ensure this is __password (double underscore)
+            # Must be __password (double underscore)
+            # Must be .lower()
             self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
-        if pwd is None or type(pwd) is not str or self.__password is None:
+        """
+        Valid password comparison
+        """
+        if pwd is None or type(pwd) is not str:
+            return False
+        if self.__password is None:
             return False
         
-        # Change .upper() to .lower() here
+        # Must match the setter (.lower())
         return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
+
+
+if __name__ == '__main__':
+    user_1 = User()
+    u_pwd = "myPassword"
+    user_1.password = u_pwd
+
+    if not user_1.is_valid_password(u_pwd):
+        print("is_valid_password should return True if it's the right password")
